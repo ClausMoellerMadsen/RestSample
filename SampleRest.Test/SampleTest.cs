f4453dfaf0df892b02635e08b2fff9b199e4b338
Claus Madsen <CLMA@bitcoinsuisse.com>
2021-05-20 21:08:20 +0200

    Add cake and unittest project

---
+++ SampleRest.Test/SampleTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SampleRest.Test
+{
+    [TestClass]
+    public class SampleTest
+    {
+        [TestMethod]
+
+        public void FailingTest()
+        {
+            Assert.IsTrue(false);
+        }
+        [TestMethod]
+
+        public void WorkingTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
